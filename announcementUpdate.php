210bc61f1d814598cadbf8f86fba2b954a739512
MITSDE <MITSDE@DESKTOP-46GVS4P.mitsde.edu>
2026-04-17 16:17:34 +0530

    change in last date

--- announcementUpdate.php
+++ announcementUpdate.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="ann-box">
-                <p style="font-size: 15.5px;">Last Day Left! Apply before <span class="blinklastdate">15th April 2026
+                <p style="font-size: 15.5px;">Last Day Left! Apply before <span class="blinklastdate">18th April 2026
                     </span> to enroll in the final batch. Call now:<a href="tel:9112207207"
                         style="text-decoration: none; padding: 0px !important;"> 9112-207-207</a> <a href="roi" style="color: white; font-weight: 400;">Calculate Your Career ROI in 30 Seconds →</a>
                     </p>
