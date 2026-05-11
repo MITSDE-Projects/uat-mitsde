9f736d951f86d66110805d239b219ad0c71ee8ba
MITSDE <MITSDE@DESKTOP-46GVS4P.mitsde.edu>
2026-05-11 10:53:58 +0530

    change in single common button add all fee course code page

--- single-common-button-add-all-fees-courses-code.php
+++ single-common-button-add-all-fees-courses-code.php
@@ -1,1 +1,2 @@
-<a class="btn btn-primary mit-button-apply-now-link" href="https://mitsde.com/new-admission-form-payment" target="blank" role="button">Enquire Now</a>
+<a class="btn btn-primary mit-button-apply-now-link" data-bs-toggle="modal"
+                data-bs-target="#enquiryModal-download-form" role="button">Enquire Now</a>
