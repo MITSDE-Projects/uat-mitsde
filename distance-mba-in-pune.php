7ca8c41eb3fd7adf596dd171d5bb5aba6c020318
MITSDE <MITSDE@DESKTOP-46GVS4P.mitsde.edu>
2026-06-11 11:37:31 +0530

    change in distance mba in pune and mumbai page h1

--- distance-mba-in-pune.php
+++ distance-mba-in-pune.php
@@ -65,7 +65,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12 col-lg-6 main-banner">
-                        <h1>Distance MBA in Pune | Executive MBA</h1>
+                        <h1>Online & Distance Learning MBA in Pune | Executive MBA</h1>
 
 
                         <div class="page-btn mt-3">
