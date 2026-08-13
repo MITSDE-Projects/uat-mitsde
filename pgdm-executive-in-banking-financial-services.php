c837beb6de95c999742228ad60af845491e178b3
MITSDE <MITSDE@DESKTOP-46GVS4P.mitsde.edu>
2026-08-13 10:47:54 +0530

    changes in new design pages create a common tab pill file

--- pgdm-executive-in-banking-financial-services.php
+++ pgdm-executive-in-banking-financial-services.php
@@ -291,18 +291,7 @@
 
     <!-- TABs -->
 
-    <div class="ph-specs-wrap sticky-pills">
-        <div class="ph-specs">
-            <!-- <a href="#kpmg"><button class="ph-spec-pill is-active">KPMG</button></a>
-            <a href="#modules"><button class="ph-spec-pill">Modules</button></a> -->
-            <a href="#faq"><button class="ph-spec-pill">FAQ</button></a>
-            <a href="#about"><button class="ph-spec-pill">About</button></a>
-            <a href="#learning"><button class="ph-spec-pill">Learning</button></a>
-            <a href="#curriculum"><button class="ph-spec-pill">Curriculum</button></a>
-            <a href="#fees"><button class="ph-spec-pill">Program Fees</button></a>
-            <a href="#courses"><button class="ph-spec-pill">Our Courses</button></a>
-        </div>
-    </div>
+    <?php include "common-tab-pills.php" ?>
 
     <!-- About Section -->
 
